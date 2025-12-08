4367a1ad6aee95b5516b64cee064cd0a92e64317
Leon <leon.gigsberg@gmail.com>
2025-12-08 15:58:04 +0700

    :fire: update image next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		// Allow loading images served from a local backend at port 8000 (use remotePatterns when port is used)
-		remotePatterns: [
-			{
-				protocol: 'http',
-				hostname: 'localhost',
-				port: '8000',
-				pathname: '/**',
-			},
-		],
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: process.env.NEXT_PUBLIC_API_HOSTNAME || 'localhost',
+        port: process.env.NEXT_PUBLIC_API_PORT || '8000',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
