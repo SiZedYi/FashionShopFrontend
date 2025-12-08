ad176da21be3caf826eb69a4687cdb5af7523bfa
Leon <leon.gigsberg@gmail.com>
2025-12-08 19:06:48 +0700

    update config for prod

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,12 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: process.env.IMG_PROTOCOL || 'http',
-        hostname: process.env.NEXT_PUBLIC_API_HOSTNAME || 'localhost',
-        port: process.env.NEXT_PUBLIC_API_PORT || '8000',
-        pathname: '/**',
+        // protocol: process.env.IMG_PROTOCOL || 'http',
+        // hostname: process.env.NEXT_PUBLIC_API_HOSTNAME || 'localhost',
+        // port: process.env.NEXT_PUBLIC_API_PORT || '8000',
+        // pathname: '/**',
+		protocol: 'https',
+        hostname: 'api.fashionshop.io.vn',
       },
     ],
   },
